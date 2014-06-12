35a7402b3b573676df088d58ed96269e1cfc76ba
Michael Helton <heltonmike@fullsail.edu>
2014-06-12 16:02:49 -0400

    dpi vs ppi
    Dots per inch into pixels per inch

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -8,3 +8,15 @@ Mike Helton
 //The Dots Per Inch being used
 var dotsPerInch = prompt ("What dpi are you using?");
 console.log(dotsPerInch);
+
+//Finding out the Pixels Per Inch
+//The Pixels Per Inch of the image
+var pixelsPerInch = prompt ("What are the pixels per inch of the image?");
+console.log(pixelsPerInch);
+
+//The rows and dots per pixel
+answer = dotsPerInch / pixelsPerInch +" Pixels = Rows and Dots per Pixel";
+console.log(answer);
+
+//Alerting the customer with the square footage
+alert(dotsPerInch / pixelsPerInch +" Pixels = Rows and Dots per Pixel");
