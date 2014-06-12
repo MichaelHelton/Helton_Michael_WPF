307b2f9002086b0de7eb8c913c65d8c86f6444b2
Michael Helton <heltonmike@fullsail.edu>
2014-06-12 10:50:35 -0400

    Finding square footage
    Finding the square footage of a room.

---
+++ Expressions_Personal/js/script.js
@@ -0,0 +1,9 @@
+/*
+Mike Helton
+06/12/2014
+Expressions_Personal
+*/
+//Finding out the square footage of a room
+//The width of the room in feet
+var roomWidth = prompt ("What is the width of your room");
+console.log(roomWidth);
