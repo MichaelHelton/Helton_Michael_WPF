d13ab15e3d9c35a4e28f73b0e589575dcccb371e
Michael Helton <heltonmike@fullsail.edu>
2014-06-12 10:57:06 -0400

    Finding square footage
    Finding the square footage of a room

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -11,3 +11,12 @@ console.log(roomWidth);
 //The length of the room in feet
 var roomLength = prompt ("What is the length of your room");
 console.log(roomLength);
+
+//The square footage
+answer = roomWidth * roomLength +" square feet";
+console.log(answer);
+
+//Alerting the customer with the square footage
+alert(roomWidth * roomLength +" square feet");
+
+
