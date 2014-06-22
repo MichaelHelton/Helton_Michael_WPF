f00c0017be4b13d484899e74a3243660f4e693f6
Michael Helton <heltonmike@fullsail.edu>
2014-06-22 15:42:35 -0400

    placed comments
    placing comments for if its clear or raining

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -7,3 +7,6 @@ Conditionals_Personal.
 
 //Finding out if we will have practice today
 var outsideWeather = prompt("Is it Clear or Raining outside?");
+
+//if it is clear we will practice
+//if it is raining we will not practice
