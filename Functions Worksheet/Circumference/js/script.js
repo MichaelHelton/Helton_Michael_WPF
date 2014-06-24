c9e653702564da9b0138c9e96ca65097a7df6c8f
Michael Helton <heltonmike@fullsail.edu>
2014-06-24 10:05:22 -0400

    prompt
    prompting the user to input size of circle

---
+++ Functions Worksheet/Circumference/js/script.js
@@ -0,0 +1,9 @@
+/**
+ Mike Helton
+ 06/24/2014
+ Functions_Worksheet_Circumference
+ */
+
+//prompting the user to input the size of their circle
+var radius = prompt("What is the size of your circle?");
+
