bdf242e447a5f7f3900b087e618ee585680d589b
Michael Helton <heltonmike@fullsail.edu>
2014-06-24 10:09:12 -0400

    console
    writing to the console

--- Functions Worksheet/Circumference/js/script.js
+++ Functions Worksheet/Circumference/js/script.js
@@ -6,4 +6,5 @@
 
 //prompting the user to input the size of their circle
 var radius = prompt("What is the size of your circle?");
-
+//writing to the console
+console.log("Your circle is " + radius);
